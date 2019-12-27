2e5a1775b5c781953e69868e36a78bd9669b05e5
Siddharth S. Tomar <37268732+siddharthst@users.noreply.github.com>
2019-12-27 16:54:14 +0100

    -

--- src/simulicronalpha/transposition.py
+++ src/simulicronalpha/transposition.py
@@ -1,19 +1,35 @@
 import numpy as np
 from numpy import random, random_intel
+from coolname import generate_slug
 import pandas as pd
 import uuid
 
 
 def transposition(TEfather, TEmother, genomeFrame, transposonFrame, generation):
-    # Skip the check for empty transposon content. Do it in the main
+    # Skip the check for empty transposon content. Do it in the parent
     GenomicSites = pd.Series(
         genomeFrame.InsertionProbability.values, index=genomeFrame.InsertionSiteID,
     )
-    GenomicSites = GenomicSites[~GenomicSites.index.isin(TEfather + TEmother)]
+    # GenomicSites = GenomicSites[~GenomicSites.index.isin(TEfather + TEmother)]
+    GenomeSites.drop(TEfather + TEmother, axis=1, inplace=True)
     for i in TEfather + TEmother:
         transpositionRate = transposonFrame.loc[transposonFrame["TID"] == i, "TraRate"]
         if transpositionRate > np.random_intel.uniform(0, 1.0):
-            insertionSite = np.choice(
-                list_of_candidates, number_of_items_to_pick, p=probability_distribution
+            insertionSite = np.random_intel.choice(
+                GenomicSites.index.values, 1, p=GenomicSites.InsertionProbability.values
+            )
+            newParent = random.choice(["M", "F"])
+            GenomeSites.drop(insertionSite, axis=1, inplace=True)
+            row = pd.Series(
+                {
+                    "TID": uuid.uuid4().hex,
+                    "InsertionSite": insertionSite,
+                    "TraRate": transpositionRate,
+                    "Name": generate_slug(),
+                    "Class": transposonFrame.loc[transposonFrame["TID"] == i, "Class"],
+                    "Traceback": [i],
+                    "Generation": 1,
+                    "Parent": newParent,
+                }
             )
     return 0
