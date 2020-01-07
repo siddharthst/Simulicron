94b287704f6f535e628747dfa5cbad8f9b2ee119
Siddharth S. Tomar <37268732+siddharthst@users.noreply.github.com>
2020-01-07 16:53:18 +0100

    Nearly finished refactoring

--- src/simulicronalpha/transposition.py
+++ src/simulicronalpha/transposition.py
@@ -5,24 +5,25 @@ import pandas as pd
 import uuid
 
 
-def transposition(genomeFrame, transposonFrame, orgFrame):
+def transposition(self, genomeFrame=None, transposonFrame=None, orgFrame=None):
     # Skip the check for empty transposon content. Do it in the parent
+    TEfather = 
+    TEmother = 
     filledSites = list(
         filter((0).__ne__, orgFrame.iloc[0]["TEfather"] + orgFrame.iloc[0]["TEmother"])
     )
     GenomicSites = pd.Series(
         genomeFrame.InsertionProbability.values, index=genomeFrame.InsertionSiteID,
-    )
-    # GenomicSites = GenomicSites[~GenomicSites.index.isin(TEfather + TEmother)]
-    GenomeSites = GenomeSites.drop(filledSites, axis=1, inplace=True)
-    for i in TEfather + TEmother:
+    )    
+    GenomicSites.drop(filledSites, axis=1, inplace=True)
+    for i in filledSites:
         transpositionRate = transposonFrame.loc[transposonFrame["TID"] == i, "TraRate"]
         if transpositionRate > np.random_intel.uniform(0, 1.0):
             insertionSite = np.random_intel.choice(
                 GenomicSites.index.values, 1, p=GenomicSites.InsertionProbability.values
             )
             newParent = random.choice(["M", "F"])
-            GenomeSites.drop(insertionSite, axis=1, inplace=True)
+            GenomicSites.drop(insertionSite, axis=1, inplace=True)
             TID = uuid.uuid4().hex
             row = pd.Series(
                 {
