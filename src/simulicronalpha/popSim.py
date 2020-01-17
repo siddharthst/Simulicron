54720250ec8c7c99854db2de16c5e23255a6908b
Siddharth S. Tomar <37268732+siddharthst@users.noreply.github.com>
2020-01-17 18:01:34 +0100

    Implemented the ultra efficent transposition function

--- src/simulicronalpha/popSim.py
+++ src/simulicronalpha/popSim.py
@@ -52,7 +52,7 @@ def generatePopulation(
 
 def generateTransposon(genomeArray, NumberOfTransposonInsertions=2):
     transposons = np.zeros(
-        (NumberOfTransposonInsertions + 1, 3), dtype=np.ndarray
+        (NumberOfTransposonInsertions + 1, 4), dtype=np.ndarray
     )
     insertionSites = np.random.choice(
         np.arange(genomeArray.shape[0]),
@@ -61,6 +61,7 @@ def generateTransposon(genomeArray, NumberOfTransposonInsertions=2):
     )
     counter = 1
     for i in insertionSites:
+        transposons[counter][3] = np.random.uniform(0.01, 0.02)
         transposons[counter][2] = genomeArray[i][0]
         transposons[counter][1] = i
         transposons[counter][0] = "%030x" % random.randrange(16 ** 30)
@@ -85,7 +86,8 @@ def generateFitness(
                 -sum(SelectionCoef)
             )
 
-def recombination(rates, transposonMatrix, v1=None, v2=None):
+
+def recombination2(rates, transposonMatrix, v1=None, v2=None):
     rec = np.random.uniform(size=len(rates)) < rates
     start = [0 if (np.random.uniform() < 0.5) else 1]
     whichcol = 1 + cumsum(c((start, rec))) % 2
@@ -108,8 +110,93 @@ def recombination(rates, transposonMatrix, v1=None, v2=None):
     elif r1.size != 0 and r2.size == 0:
         return r1
     else:
-        return (c([r1, r2]))
+        return c([r1, r2])
 
 
-def transposition():
-    return 0
+def recombination(rates, transposonMatrix, v1=None, v2=None):
+    rec = np.random.uniform(size=len(rates)) < rates
+    start = [0 if (np.random.uniform() < 0.5) else 1]
+    whichcol = 1 + cumsum(c((start, rec))) % 2
+    allele1 = set(np.where(whichcol == 1)[0])
+    allele2 = set(np.where(whichcol == 2)[0])
+    transposon1 = transposonMatrix[[v1], 1].tolist()[0]
+    transposon2 = transposonMatrix[[v2], 1].tolist()[0]
+    haplotype1 = [x in allele1 for x in transposon1]
+    haplotype2 = [x in allele2 for x in transposon2]
+    r1 = v1[haplotype1]
+    r2 = v2[haplotype2]
+    if r1.size == 0 and r2.size == 0:
+        return 0
+    elif r1.size == 0 and r2.size != 0:
+        return r2
+    elif r1.size != 0 and r2.size == 0:
+        return r1
+    else:
+        return c([r1, r2])
+
+
+def transposition(transposonMatrix, genomeMatrix, v1=None, v2=None):
+    if v1 == 0:
+        allele1 = []
+        allele1Sites = []
+        allele1Index = []
+    else:
+        allele1 = transposonMatrix[[v1], 3].tolist()[0]
+        allele1Sites = transposonMatrix[[v1], 1].tolist()[0]
+        allele1Index = v1
+    if v2 == 0:
+        allele2 = []
+        allele2Sites = []
+        allele2Index = []
+    else:
+        allele2 = transposonMatrix[[v2], 3].tolist()[0]
+        allele2Sites = transposonMatrix[[v2], 1].tolist()[0]
+        allele2Index = v2
+
+    transposonIndices =  np.array(allele1Index + allele2Index)
+    transposonRates = np.array(allele1 + allele2)
+    filledSites = allele1Sites + allele2Sites
+    Transoposecheck = transposonRates > np.random.uniform(
+        0, 0.1, len(transposonRates)
+    )
+
+    if not any(Transoposecheck):
+        return (v1, v2)
+    else:
+        transposonsToTranspose = transposonIndices[Transoposecheck]
+        emptySiteIndices = [
+            x
+            for x in list(range(len(genomeMatrix)))
+            if x not in filledSites
+        ]
+        emptySitesProb = genomeMatrix[emptySiteIndices, 1].tolist()
+        probSum = sum(emptySitesProb)
+        InsertionProb = [float(i) / probSum for i in emptySitesProb]
+        sites = np.random.choice(
+            emptySiteIndices,
+            size=sum(Transoposecheck),
+            replace=False,
+            p=InsertionProb,
+        )
+        progenyAllele = random.choices(
+            ["v1", "v2"], k=sum(Transoposecheck)
+        )
+        for i in list(range(len(transposonsToTranspose))):
+            transposonToAdd = [
+                "%030x" % random.randrange(16 ** 30),
+                sites[i],
+                genomeMatrix[sites[i]][0],
+                transposonMatrix[transposonsToTranspose[i], 3],
+            ]
+            transposonMatrix = np.vstack(
+                [transposonMatrix, transposonToAdd,]
+            )
+            if progenyAllele[i] == "v1":
+                allele1Index.append(len(transposonMatrix)-1)
+            if progenyAllele[i] == "v2":
+                allele2Index.append(len(transposonMatrix)-1)
+    if (allele1Index == []):
+        allele1Index = 0
+    if (allele2Index == []):
+        allele2Index = 0            
+    return (allele1Index, allele2Index)
