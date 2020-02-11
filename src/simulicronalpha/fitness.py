86535ca53f6b9aee8d5b2e26e506ae9dda4ae982
Siddharth S. Tomar <37268732+siddharthst@users.noreply.github.com>
2020-02-11 17:29:42 +0100

    Fixed issue #1

--- src/simulicronalpha/fitness.py
+++ src/simulicronalpha/fitness.py
@@ -3,23 +3,6 @@ from numpy import cumsum
 from numpy import concatenate as c
 import random
 
-def generateFitness(
-    populationMatrix, transposonMatrix, fitnessFunction=1
-):
-    allele1Index = np.nonzero(populationMatrix[0:, 0])[0]
-    allele2Index = np.nonzero(populationMatrix[0:, 1])[0]
-    for i in np.concatenate((allele1Index, allele2Index), axis=0):
-        transposonContent = np.trim_zeros(
-            np.hstack(
-                [populationMatrix[i][0]] + [populationMatrix[i][1]]
-            )
-        )
-        SelectionCoef = transposonMatrix[transposonContent, 2]
-        if fitnessFunction == 1:
-            populationMatrix[i][2] = np.exp(sum(SelectionCoef))
-    return populationMatrix[:, 2]
-
-
 def calculateFitness(
     transposonMatrix, v1, v2, fitnessFunction=1,
 ):
