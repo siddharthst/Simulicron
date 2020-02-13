f119ef3ecdfa21a3ea7e4d66d31692e3545f5de2
Siddharth S. Tomar <37268732+siddharthst@users.noreply.github.com>
2020-02-13 15:53:31 +0100

    Working on a program breaking bug associated with numpy vstack

--- src/simulicronalpha/popSim.py
+++ src/simulicronalpha/popSim.py
@@ -3,7 +3,11 @@ from numpy import cumsum
 from numpy import concatenate as c
 import random
 
-from generateSim import generateGenome, generatePopulation, generateTransposon
+from generateSim import (
+    generateGenome,
+    generatePopulation,
+    generateTransposon,
+)
 from recombination import recombination
 from fitness import calculateFitness
 from transposition import transposition
@@ -28,7 +32,11 @@ def runSim(
     # ------------------#
     # lambda/macros
     flatten = lambda *n: (
-        e for a in n for e in (flatten(*a) if isinstance(a, (tuple, list)) else (a,))
+        e
+        for a in n
+        for e in (
+            flatten(*a) if isinstance(a, (tuple, list)) else (a,)
+        )
     )
     # ------------------#
     # ------------------#
@@ -49,13 +57,18 @@ def runSim(
             fitness = list(populationMatrixCopy[0:, 2])
 
             p1, p2 = random.choices(
-                list(range(populationMatrixCopy.shape[0])), weights=fitness, k=2
+                list(range(populationMatrixCopy.shape[0])),
+                weights=fitness,
+                k=2,
             )
 
             # Since recombination function only accepts arrays,
             # checking and forcing type conversion as needed
             # for the respective alleles
-            if populationMatrixCopy[p1, 0] == 0 and populationMatrixCopy[p1, 1] == 0:
+            if (
+                populationMatrixCopy[p1, 0] == 0
+                and populationMatrixCopy[p1, 1] == 0
+            ):
                 v1 = 0
             else:
                 cP1V1 = populationMatrixCopy[p1, 0]
@@ -70,10 +83,16 @@ def runSim(
                     cP1V2 = np.asarray([cP1V2])
 
                 v1 = recombination(
-                    genomeMatrix[0:, 2], transposonMatrixCopy, v1=cP1V1, v2=cP1V2
+                    genomeMatrix[0:, 2],
+                    transposonMatrixCopy,
+                    v1=cP1V1,
+                    v2=cP1V2,
                 )
 
-            if populationMatrixCopy[p2, 0] == 0 and populationMatrixCopy[p2, 1] == 0:
+            if (
+                populationMatrixCopy[p2, 0] == 0
+                and populationMatrixCopy[p2, 1] == 0
+            ):
                 v2 = 0
             else:
                 cP2V1 = populationMatrixCopy[p2, 0]
@@ -88,7 +107,10 @@ def runSim(
                     cP2V2 = np.asarray([cP2V2])
 
                 v2 = recombination(
-                    genomeMatrix[0:, 2], transposonMatrixCopy, v1=cP2V1, v2=cP2V2
+                    genomeMatrix[0:, 2],
+                    transposonMatrixCopy,
+                    v1=cP2V1,
+                    v2=cP2V2,
                 )
 
             if v1 == 0 and v2 == 0:
@@ -103,7 +125,9 @@ def runSim(
                     v1=v1,
                     v2=v2,
                 )
-                indFitness = calculateFitness(transposonMatrixCopy, v1, v2)
+                indFitness = calculateFitness(
+                    transposonMatrixCopy, v1, v2
+                )
 
             populationV1.append(v1)
             populationV2.append(v2)
@@ -113,7 +137,10 @@ def runSim(
         # n-1, hence i + 1 + 1
 
         # Check if there are no transposons left
-        if all(np.array_equal(v, [0, 0]) for v in np.c_[populationV1, populationV2]):
+        if all(
+            np.array_equal(v, [0, 0])
+            for v in np.c_[populationV1, populationV2]
+        ):
             return (
                 "LOSS",
                 fixedTE,
@@ -125,7 +152,7 @@ def runSim(
 
         # Check if all members of population contain transposon
         if not any(
-            all(k == 0 for k in z)
+            any(k == 0 for k in z)
             for z in [
                 list(flatten(v.flatten().tolist()))
                 for v in np.c_[populationV1, populationV2]
@@ -134,13 +161,13 @@ def runSim(
             counter = 0
             for TE in TEset.keys():
                 if all(
-                    bool(set(k).intersection(TEset[i]))
+                    bool(set(k).intersection(TEset[TE]))
                     for k in populationV1 + populationV2
                 ):
                     fixedTE.append(TE)
                     counter += 1
                 elif any(
-                    bool(set(k).intersection(TEset[i]))
+                    bool(set(k).intersection(TEset[TE]))
                     for k in populationV1 + populationV2
                 ):
                     unfixedTE.append(i)
@@ -160,7 +187,18 @@ def runSim(
                     )
 
         # Bind population for next iteration
-        populationMatrixCopy = np.vstack((populationV1, populationV2, populationFit)).T
+        print ("++++++++++++++++++++")
+        print (len(populationV1))
+        print (len(populationV2))
+        print (len(populationFit))
+        print ("--------------------")
+        print (populationV1)
+        print (populationV2)
+        print (populationFit)
+        print ("....................")
+        populationMatrixCopy = np.vstack(
+            (populationV1, populationV2, populationFit)
+        ).T
 
     # Quit simulation if there in a transient state
     # i.e. no fixation or loss
@@ -174,7 +212,8 @@ def runSim(
     )
 
 
-def runBatch(
+# Creating a generator for the dataset
+def createData(
     numberOfSimulations=1000,
     numberOfChromosomes=4,
     numberOfInsertionSites=1000,
@@ -184,40 +223,10 @@ def runBatch(
     NumberOfGenerations=100000,
     baseSelection=1,
     baseTransposition=1,
-    testCondition=None,
     consecutiveTransposons=False,
     changeRecombination=False,
     baseTrRecombination=0.1,
 ):
-    print("Supplied parameters: ")
-    print("Number of simulations          : ", numberOfSimulations)
-    print("Number of generations          : ", NumberOfGenerations)
-    print("Number Of Individual           : ", NumberOfIndividual)
-    print("Number Of Insertion Sites      : ", numberOfInsertionSites)
-    print("Number Of Chromosomes          : ", numberOfChromosomes)
-    print("Number Of transposon insertion : ", NumberOfTransposonInsertions)
-    print("----------------------------------- :")
-    print("Sample genome with given parameters :")
-    gen = generateGenome(
-        numberOfInsertionSites=numberOfInsertionSites,
-        numberOfChromosomes=numberOfChromosomes,
-        baseRecombinationRate=baseRecombinationRate,
-        baseSelection=baseSelection,
-    )
-    print(gen)
-    print("--------------------------------------- :")
-    print("Sample transposon with given parameters :")
-    tr, gen = generateTransposon(
-        genomeArray=gen,
-        NumberOfTransposonInsertions=NumberOfTransposonInsertions,
-        baseTransposition=baseTransposition,
-        consecutiveTransposons=consecutiveTransposons,
-        changeRecombination=changeRecombination,
-        RecombinationRate=baseTrRecombination,
-    )
-    print(tr)
-
-    argArray = []
     for i in range(numberOfSimulations):
         gen = generateGenome(
             numberOfInsertionSites=numberOfInsertionSites,
@@ -239,10 +248,51 @@ def runBatch(
             NumberOfTransposonInsertions=NumberOfTransposonInsertions,
         )
 
-        argArray.append((gen, pop, tr, TEset))
+        yield (
+            (
+                gen,
+                pop,
+                tr,
+                TEset,
+                NumberOfTransposonInsertions,
+            )
+        )
 
-    with multiprocessing.Pool(processes=4) as pool:
-        results = pool.starmap(runSim, argArray)
+
+def runBatch(
+    numberOfSimulations=1000,
+    numberOfChromosomes=4,
+    numberOfInsertionSites=1000,
+    baseRecombinationRate=0.1,
+    NumberOfIndividual=1000,
+    NumberOfTransposonInsertions=2,
+    NumberOfGenerations=100000,
+    baseSelection=1,
+    baseTransposition=1,
+    consecutiveTransposons=False,
+    changeRecombination=False,
+    baseTrRecombination=0.1,
+):
+    dataSet = createData(
+        numberOfSimulations,
+        numberOfChromosomes,
+        numberOfInsertionSites,
+        baseRecombinationRate,
+        NumberOfIndividual,
+        NumberOfTransposonInsertions,
+        NumberOfGenerations,
+        baseSelection,
+        baseTransposition,
+        consecutiveTransposons,
+        changeRecombination,
+        baseTrRecombination,
+    )
+    inputSet = []
+    for i in dataSet:
+        inputSet.append(i) 
+
+    with multiprocessing.Pool(processes=1) as pool:
+        results = pool.starmap(runSim, inputSet)
 
     return results
 
