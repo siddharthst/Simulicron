4d24af4cf953af1e39edf69668f9053f8d45e42b
Siddharth S. Tomar <37268732+siddharthst@users.noreply.github.com>
2020-06-22 12:55:03 +0200

    -

---
+++ Other/prevsrc/22Jun2020/recombination.py
@@ -0,0 +1,65 @@
+import numpy as np
+from numpy import cumsum
+from numpy import concatenate as c
+import random
+
+
+def recombination(rates, transposonMatrix, v1, v2):
+    # Empty vectors to store result
+    r1 = []
+    r2 = []
+    # Creating lambda (macro)
+    # "Match" does not exist in python
+    # match = lambda a, b: [ b.index(x) if x in b else 0 for x in a ]
+    # Get the postion of transposons
+    positionV1 = transposonMatrix[v1, 1].astype(int).tolist()
+    positionV2 = transposonMatrix[v2, 1].astype(int).tolist()
+    # This step sorts the locations and adds another location,
+    # 0 if not already present
+    unqiquePos = list(set(positionV1 + positionV2) | set([0]))
+    unqiquePos.sort()
+    # Calculate the effective rate from genome map
+    effectiveRates = 0.5 * (1 - np.exp(-2 * np.diff(rates[unqiquePos])))
+    # print (effectiveRates)
+    # Performing "Recombination"
+    rec = np.random.uniform(size=len(effectiveRates)) < effectiveRates
+    # Select the direction to start from
+    start = [(np.random.uniform() < 0.5)]
+    # Concat. start and recombination
+    # Also remove the added 0 and start from
+    # whichhaplo and uniqpos
+    whichhaplo = 1 + cumsum(c((start, rec))) % 2
+    whichhaplo = np.delete(whichhaplo, 0)
+    del unqiquePos[0]
+    unqiquePos = np.asarray(unqiquePos)
+    # Generating the haplotype
+    # Also checking if there is no transposon left
+    # In the case above, return a (int) 0
+    # Else return the array containing transposons
+    if positionV1 == [0]:
+        r1 = []
+    else:
+        if not any(whichhaplo == 1):
+            pass
+        else:
+            pos = unqiquePos[whichhaplo == 1]
+            for i in v1:
+                if int(transposonMatrix[i, 1]) in pos:
+                    r1.append(i)
+    if positionV2 == [0]:
+        r2 = []
+    else:
+        if not any(whichhaplo == 2):
+            pass
+        else:
+            pos = unqiquePos[whichhaplo == 2]
+            for i in v2:
+                if int(transposonMatrix[i, 1]) in pos:
+                    r2.append(i)
+    # Merge to create gamate
+    r = r1 + r2
+    # Return 0 if no transposon remains
+    if not r:
+        return 0
+    else:
+        return r
--- src/simulicronalpha/transposition.py
+++ src/simulicronalpha/transposition.py
@@ -8,109 +8,10 @@ import random
 def transposition(
     transposonMatrix, genomeMatrix, NumberOfTransposonInsertions, TEset, v1, v2,
 ):
-    if v1 == 0:
-        allele1 = []
-        allele1Sites = []
-        allele1Index = []
-        allele1ExicsionRate = []
-        allele1RepairRate = []
-        allele1InsertionRate = []
-    else:
-        allele1ExicsionRate = transposonMatrix[[v1], 3].tolist()[0]
-        allele1RepairRate = transposonMatrix[[v1], 4].tolist()[0]
-        allele1InsertionRate = transposonMatrix[[v1], 5].tolist()[0]
-        allele1Sites = transposonMatrix[[v1], 1].astype(int).tolist()[0]
-        allele1Index = v1
-
-    if v2 == 0:
-        allele2 = []
-        allele2Sites = []
-        allele2Index = []
-        allele2ExicsionRate = []
-        allele2RepairRate = []
-        allele2InsertionRate = []
-
-    else:
-        allele2ExicsionRate = transposonMatrix[[v2], 3].tolist()[0]
-        allele2RepairRate = transposonMatrix[[v2], 4].tolist()[0]
-        allele2InsertionRate = transposonMatrix[[v2], 5].tolist()[0]
-        allele2Sites = transposonMatrix[[v2], 1].astype(int).tolist()[0]
-        allele2Index = v2
-
-    transposonIndices = np.array(allele1Index + allele2Index)
-    # Find effective ExcisionRates
-    transposonExcisionRates = regulation(
-        transposons=transposonIndices,
-        TEset=TEset,
-        transposonMatrix=transposonMatrix,
-        genomeMatrix=genomeMatrix,
-    )
-    transposonRepairRates = np.array(allele1RepairRate + allele2RepairRate)
-    transposonInsertionRates = np.array(allele1InsertionRate + allele2InsertionRate)
-
-    exicsionCheck = transposonExcisionRates > np.random.uniform(
-        0, 1, len(transposonExcisionRates)
-    )
-    repairCheck = transposonRepairRates > np.random.uniform(
-        0, 1, len(transposonRepairRates)
-    )
-    insertionCheck = transposonInsertionRates > np.random.uniform(
-        0, 1, len(transposonInsertionRates)
-    )
-    # Find the intersection
-    # Transoposecheck = [
-    #     x for x in exicsionCheck if x in repairCheck and x in insertionCheck
-    # ]
-    Transoposecheck = exicsionCheck & repairCheck & insertionCheck
-    # Return alleles as they are if no transposition happens
-    if not any(Transoposecheck):
-        return (v1, v2, transposonMatrix, TEset)
-    else:
-        transposonsToTranspose = transposonIndices[Transoposecheck]
-        # If transposons share the same site - replace the old transposon
-        # with new transposon
-        # Choose the allele for tranposition
-        progenyAllele = random.choices(["v1", "v2"], k=sum(Transoposecheck))
-        # probSum = sum(emptySitesProb)
-        # InsertionProb = [float(i) / probSum for i in emptySitesProb]
-        sites = np.random.choice(list(range(len(genomeMatrix))), size=sum(Transoposecheck), replace=False)
-        for i in list(range(len(transposonsToTranspose))):
-            transposonToAdd = [
-                0,
-                sites[i],
-                genomeMatrix[sites[i]][0],
-                transposonMatrix[transposonsToTranspose[i], 3],
-                transposonMatrix[transposonsToTranspose[i], 4],
-                transposonMatrix[transposonsToTranspose[i], 5],
-            ]
-
-            transposonMatrix = np.vstack(
-                [transposonMatrix, np.asarray(transposonToAdd, object),]
-            )
             # Code to track the genealogy of TE
             for k in range(NumberOfTransposonInsertions):
                 if transposonsToTranspose[i] in TEset[k + 1]:
                     TEset[k + 1].add(len(transposonMatrix) - 1)
                     pass
-
-            # Assign TE to the choosen allele
-            if progenyAllele[i] == "v1":
-                # First check if the transposon is replacing another transposon on same allele
-                # If yes, we need to remove the replaced transposon from the said allele
-                if sites[i] in allele1Sites:
-                    vIndex = allele1Sites.index(sites[i])
-                    del allele1Index[vIndex]
-                allele1Index.append(len(transposonMatrix) - 1)
-            if progenyAllele[i] == "v2":
-                # Same as above
-                if sites[i] in allele2Sites:
-                    vIndex = allele2Sites.index(sites[i])
-                    del allele2Index[vIndex]
-                allele2Index.append(len(transposonMatrix) - 1)
-
-    if allele1Index == []:
-        allele1Index = 0
-    if allele2Index == []:
-        allele2Index = 0
 
     return (allele1Index, allele2Index, transposonMatrix, TEset)
