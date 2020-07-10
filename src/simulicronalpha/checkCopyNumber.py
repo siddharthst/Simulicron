1150f2b67c7cb6fd1e9156f6fe3ce2f19ae6f0d0
Siddharth S. Tomar <siddharth@localhost.localdomain>
2020-07-10 11:14:34 +0200

    Fixed bug with copynumber check

--- src/simulicronalpha/checkCopyNumber.py
+++ src/simulicronalpha/checkCopyNumber.py
@@ -26,4 +26,4 @@ def checkCopyNumber(populationMatrix, TEset, transposonMatrix):
     for k in TEset.keys():
         TEfamilyVar[k].append(np.var(TEfamilyCount[k]))
         TEfamilyCount[k] = sum(TEfamilyCount[k])
-    return sum(TETotal), np.var(TETotal), TEfamilyCount, TEfamilyVar
+    return sum(TETotal)/len(populationMatrix), np.var(TETotal), TEfamilyCount, TEfamilyVar
