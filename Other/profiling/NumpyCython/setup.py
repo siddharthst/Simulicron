aa9a7be85820ebfd4441ec2b5e4b58198b7725d9
Siddharth S. Tomar <siddharth.tomar@egce.cnrs-gif.fr>
2022-02-01 13:35:00 +0100

    Pre-publish commit

--- Other/profiling/NumpyCython/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
-
-setup(
-  name='Recombination function in Cython',
-  ext_modules=[Extension('_Cyrecombination_', ['Cyrecombination.pyx'],)],
-  cmdclass={'build_ext': build_ext},
-)
