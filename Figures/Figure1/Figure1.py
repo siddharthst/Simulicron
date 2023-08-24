fc44d1b83cb6a98f52028efc47a6e976ec0d4e6f
Arnaud Le Rouzic <lerouzic@egce.cnrs-gif.fr>
2023-08-24 15:05:07 +0200

    Rewriting Figure1.py; now similar to Figure2.py.

--- Figures/Figure1/Figure1.py
+++ Figures/Figure1/Figure1.py
@@ -15,6 +15,7 @@ import warnings
 import pickle
 from numpy import concatenate as c
 from itertools import repeat
+import copy
 
 # Simulation imports
 from popSim import runSim
@@ -32,115 +33,102 @@ import concurrent.futures
 
 # Wrapper function for multiprocessing
 def worker(parameters):
+	
     np.random.seed(int.from_bytes(os.urandom(4), byteorder='little'))
+    
     # Generate genome and population
-    selectionCoef = np.random.uniform(
-        parameters["selectionPenaltyMin"],
-        parameters["selectionPenaltyMax"],
-    )
-    FrequencyOfInsertionMain = np.random.uniform(
-        parameters["FrequencyOfInsertionMainMin"],
-        parameters["FrequencyOfInsertionMainMax"],
-    )
-    FrequencyOfInsertionHGT = np.random.uniform(
-        parameters["FrequencyOfInsertionHGTMin"],
-        parameters["FrequencyOfInsertionHGTMax"],
-    )
-    ExcisionRateMain = np.random.uniform(
-        parameters["ExcisionRateMainMin"],
-        parameters["ExcisionRateMainMax"],
-    )
-    ExcisionRateHGT = np.random.uniform(
-        parameters["ExcisionRateHGTMin"],
-        parameters["ExcisionRateHGTMax"],
-    )
-    HMTgen = random.randint(
-        parameters["HGTgenerationMin"],
-        parameters["HGTgenerationMax"],
-    )
-    tau = np.random.uniform(
-        parameters["tauMin"], parameters["tauMax"],
-    )
-    eta = np.random.uniform(
-        parameters["etaMin"], parameters["etaMax"],
-    )
-    epistasisCoefficient = np.random.uniform(
-        parameters["epistasisCoefficientMax"], parameters["epistasisCoefficientMin"],
-    )
     genome, piset, piIndice, rates = generateGenome(
-        numberOfInsertionSites=1000,
-        numberOfChromosomes=5,
-        baseRecombinationRate=0.1,
-        baseSelection=selectionCoef,
-        baseTau=tau,
+        numberOfInsertionSites = parameters["NumberOfInsertionSites"],
+        numberOfChromosomes    = parameters["NumberOfChromosomes"],
+        baseRecombinationRate  = parameters["RecombinationRate"],
+        baseSelection          = parameters["selectionPenalty"],
+        baseTau                = parameters["tau"],
+        disablePiRecombination = True,
     )
+        
     population, transposons, TEset = generatePopulation(
         genome,
         piIndice,
-        NumberOfIndividual=parameters["individuals"],
-        NumberOfTransposonTypes=2,
-        NumberOfInsertionsPerType=[1, 0],
-        FrequencyOfInsertions=[
-            FrequencyOfInsertionMain,
-            FrequencyOfInsertionHGT,
+        NumberOfIndividual     = parameters["Individuals"],
+        NumberOfTransposonTypes = 2,
+        NumberOfInsertionsPerType = [1, 0],
+        FrequencyOfInsertions  =[
+            parameters["FrequencyOfInsertionMain"],
+            parameters["FrequencyOfInsertionHGT"],
         ],
-        ExcisionRates=[ExcisionRateMain, ExcisionRateHGT],
-        RepairRates=[1, 1],
-        InsertionRates=[1, 1],
+        ExcisionRates          = [
+            parameters["ExcisionRateMain"], 
+            parameters["ExcisionRateHGT"],
+        ],
+        RepairRates            = [1, 1],
+        InsertionRates         = [1, 1],
     )
+    
+    # run the simulation
     result = runSim(
-        genomeMatrix=genome,
-        populationMatrix=population,
-        transposonMatrix=transposons,
-        TEset=TEset,
+        genomeMatrix           = genome,
+        populationMatrix       = population,
+        transposonMatrix       = transposons,
+        TEset                  = TEset,
         NumberOfTransposonInsertions=2,
-        generations=parameters["generations"],
-        genMap=rates,
-        piRNAindices=piIndice,
-        simHGT=None,
-        HMTgen=HMTgen,
-        NumberOfInsertionsPerType=None,
-        FrequencyOfInsertions=[
-            FrequencyOfInsertionMain,
-            FrequencyOfInsertionHGT,
+        generations            = parameters["Generations"],
+        genMap                 = rates,
+        piRNAindices           = piIndice,
+        simHGT                 = None,
+        HMTgen                 = parameters["HGTgeneration"],
+        NumberOfInsertionsPerType = None,
+        FrequencyOfInsertions  =[
+            parameters["FrequencyOfInsertionMain"],
+            parameters["FrequencyOfInsertionHGT"],
         ],
-        ExcisionRates=[ExcisionRateMain, ExcisionRateHGT],
-        RepairRates=[1, 1],
-        InsertionRates=[1, 1],
-        eta=eta,
-        tau=tau,
-        selPen=selectionCoef,
-        epistasisCoefficient = epistasisCoefficient,
-        fitnessFunction = 2,
+        ExcisionRates          = [
+            parameters["ExcisionRateMain"], 
+            parameters["ExcisionRateHGT"],
+        ],
+        RepairRates            = [1, 1],
+        InsertionRates         = [1, 1],
+        eta                    = parameters["eta"],
+        tau                    = parameters["tau"],
+        selPen                 = parameters["selectionPenalty"],
+        epistasisCoefficient   = parameters["epistasisCoefficient"],
+        fitnessFunction        = 2
     )
-    with open("./Results/"+'%030x' % random.randrange(16**30) + parameters["saveSuffix"] + ".pickle", "wb") as f:
+    
+    ff = "./Results/"+'%030x' % random.randrange(16**30) + "-" + parameters["saveSuffix"] + ".pickle"
+    with open(ff, "wb") as f:
         pickle.dump((result), f)
+        
+    return ff
 
-    return 0
 
 with open('../../Default.parameters', 'r') as file:
     parameters = json.load(file)
 
-# General simulations
-parameters["HGTgenerationMax"] = 999
-parameters["HGTgenerationMin"] = 1
+
+parameters["Generations"] = 100
+parameters["Individuals"] = 100
+parameters["NumberOfInsertionSites"] = 20
+
+etas          = np.arange(0, 1.0001, 0.05).tolist()
+HTgenerations = np.arange(0, 200.01, 10).tolist()
+
+replicates    = 3
+
+def makepar(pp):
+    # pp[0] is HTgen, pp[1] is eta
+    mypar = copy.deepcopy(parameters) # Otherwise the reference only is copied
+    mypar["HGTgeneration"] = pp[0]
+    mypar["eta"]           = pp[1]
+    mypar["saveSuffix"]    = "HT" + str(pp[0]) + f"-eta{pp[1]:.2f}"	
+    return mypar
+	
+allpar = [makepar([h,e]) for h in HTgenerations for e in etas]
+allparrep = sum(list(repeat(allpar,replicates)), [])
 
 with concurrent.futures.ProcessPoolExecutor(max_workers=parameters["maxProcceses"]) as executor:
-    futures = [executor.submit(worker, arg) for arg in repeat(parameters,2000)]
+    futures = [executor.submit(worker, arg) for arg in allparrep]
     for future in concurrent.futures.as_completed(futures):
-        print (future)
+                print (future.result())
 
-# Set the four corners
-epistasis = [parameters["etaMin"],parameters["etaMax"]]
-generationOfInfection = [0,1000]
-for i in epistasis:
-    for k in generationOfInfection:
-        parameters["etaMin"] = i
-        parameters["etaMax"] = i
-        parameters["HGTgenerationMin"] = k
-        parameters["HGTgenerationMax"] = k
-        with concurrent.futures.ProcessPoolExecutor(max_workers=parameters["maxProcceses"]) as executor:
-            futures = [executor.submit(worker, arg) for arg in repeat(parameters,250)]
-            for future in concurrent.futures.as_completed(futures):
-                print (future)
+
 
