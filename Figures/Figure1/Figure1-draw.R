bbc39979500170cbba475722cdb76331e2392414
Arnaud Le Rouzic <lerouzic@egce.cnrs-gif.fr>
2023-08-29 17:02:50 +0200

    Reproducing the original figure + a couple of tries.

--- Figures/Figure1/Figure1-draw.R
+++ Figures/Figure1/Figure1-draw.R
@@ -14,9 +14,6 @@ sapply <- function (X, FUN, ..., simplify = TRUE, USE.NAMES = TRUE)
     else answer
 }
 
-
-
-
 res.dir <- "./Results"
 
 get.HTgen <- function(filenames) {
@@ -41,7 +38,15 @@ max.copy.number <- function(filenames) {
     }))
 }
 
-zlim <- c(-3, 3)
+constrain <- function(x, range) {
+    ifelse( x > range[2], range[2], ifelse(x < range[1], range[1], x))
+}
+
+zlim <- list(
+    logratio = c(-0.5, 3), 
+    alpha    = c(0, 50),
+    beta     = c(0, 50))
+    
 ncol <- 1024
 
 grad <- colorRampPalette(c("yellow", "darkgreen"))(ncol+1)
@@ -57,17 +62,39 @@ dd <- data.frame(
     max.alpha = max.cp[,1],
     max.beta  = max.cp[,2]
 )
-dd$logratio <- dd$max.beta / dd$max.alpha
+dd$logratio <- dd$max.alpha / dd$max.beta
 
 # in case of replicates: take the mean
 ddm <- aggregate(. ~ HTgen + eta, dd[,-1], mean)
 
-toplot <- dd$logratio
-toplot[toplot < zlim[1]] <- zlim[1]
-toplot[toplot > zlim[2]] <- zlim[2]
-
-colplot <- grad[1+round(ncol*((toplot-zlim[1])/diff(zlim)))]
-
 # Main panel
+pdf("Figure1.pdf", width=8, height=5)
+    layout(t(1:2), width=c(0.8,0.2))
+    
+    colplot.logratio <- grad[1+round(ncol*((constrain(ddm$logratio, zlim[["logratio"]])-zlim[["logratio"]][1])/diff(zlim[["logratio"]])))]
+    
+    plot(ddm$eta, ddm$HTgen, xlab=expression("Cross-regulation coefficient ("*eta*")"), ylab=expression("Introduction of TE "*beta*"(H)"), pch=19, col=colplot.logratio, cex=2, main=expression("Max("*alpha*")/Max("*beta*")"))
+    
+    image(x=1, y=seq(zlim[["logratio"]][1], zlim[["logratio"]][2], length=length(grad)-1), z=t(seq(zlim[["logratio"]][1], zlim[["logratio"]][2], length=length(grad)-1)), col=grad, xlab="", ylab=expression("Max("*alpha*")/Max("*beta*")"), xaxt="n", yaxt="n")
+    axis(2, at=log(c(1, 2, 5, 10)), labels=c(1,2,5,10))
+dev.off()
 
-plot(dd$eta, dd$HTgen, xlab=expression(eta), ylab="H", pch=19, col=colplot, cex=2)
+pdf("Figure1-alpha.pdf", width=8, height=5)
+    layout(t(1:2), width=c(0.8,0.2))
+    
+    colplot.alpha <- grad[1+round(ncol*((constrain(ddm$max.alpha, zlim[["alpha"]])-zlim[["alpha"]][1])/diff(zlim[["alpha"]])))]
+    
+    plot(ddm$eta, ddm$HTgen, xlab=expression("Cross-regulation coefficient ("*eta*")"), ylab=expression("Introduction of TE "*beta*"(H)"), pch=19, col=colplot.alpha, cex=2, main=expression("Resident TE"))
+    
+    image(x=1, y=seq(zlim[["alpha"]][1], zlim[["alpha"]][2], length=length(grad)-1), z=t(seq(zlim[["alpha"]][1], zlim[["alpha"]][2], length=length(grad)-1)), col=grad, xlab="", ylab=expression("Max("*alpha*")"), xaxt="n")
+dev.off()
+
+pdf("Figure1-beta.pdf", width=8, height=5)
+    layout(t(1:2), width=c(0.8,0.2))
+    
+    colplot.beta <- grad[1+round(ncol*((constrain(ddm$max.beta, zlim[["beta"]])-zlim[["beta"]][1])/diff(zlim[["beta"]])))]
+
+    plot(ddm$eta, ddm$HTgen, xlab=expression("Cross-regulation coefficient ("*eta*")"), ylab=expression("Introduction of TE "*beta*"(H)"), pch=19, col=colplot.beta, cex=2, main=expression("Invading TE"))
+    
+    image(x=1, y=seq(zlim[["beta"]][1], zlim[["beta"]][2], length=length(grad)-1), z=t(seq(zlim[["beta"]][1], zlim[["beta"]][2], length=length(grad)-1)), col=grad, xlab="", ylab=expression("Max("*beta*")"), xaxt="n")
+dev.off()
